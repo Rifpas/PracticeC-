ff606f4927d74ea569aed76919481d1915d9f482
Ruslan Kurmaz <rkurmaz@ya.ru>
2023-02-26 01:02:56 +0500

    решение задач Task05 и 07

---
+++ Task05/Program.cs
@@ -0,0 +1,15 @@
+﻿//принимает на входе число N, а на выходе выводит все целые числа от -N до N
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    for (int i = -number; i <= number; i++)
+    {
+        Console.Write(i + " ");
+    }
+}
+else
+{
+    Console.Write("Число должно быть положительным");
+}
