e75cda5a3c801591bd019b5a69f38baca1a6f95c
Ruslan Kurmaz <rkurmaz@ya.ru>
2023-02-25 23:52:29 +0500

    Решение задач Task 01 и 02

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Является ли первое число квадратом второго числа
+Console.WriteLine("Введите целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int square = numberB * numberB;
+if (square == numberA)
+{
+    Console.WriteLine(square);
+
+}
+else 
+{
+    Console.Write("нет");
+}
---
+++ Task03/Program.cs
@@ -0,0 +1,20 @@
+﻿// присвоение день недели к числу
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+    if (number == 1)
+{
+    Console.WriteLine("Monday");
+}
+    if (number == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+    if (number == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+else 
+    if (number > 3)
+    {
+        Console.WriteLine("Введите число не больше 3");
+    }
