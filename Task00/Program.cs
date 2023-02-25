cb420fd207b00f2937bdbda948750cd12a2fcad6
Ruslan Kurmaz <rkurmaz@ya.ru>
2023-02-25 22:35:53 +0500

    Задача возведение числа в квадрат

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. На вход принимает число
+// 2. выдает его квадрат(число умноженное само на себя)
+
+
+
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;                               // square - возведение в квадрат
+Console.WriteLine($"Квадрат числа {number} = {square}");   // интраполяция строки с помощью $ и {}, позволяет из просто текста превратить изменяемые переменные
