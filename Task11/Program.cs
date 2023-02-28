6ce0245823304a3df1d9e08c8f840b19c5d5fa77
Ruslan Kurmaz <rkurmaz@ya.ru>
2023-02-28 22:47:43 +0500

    add Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,1 +1,21 @@
-﻿int number = new Random().Next(100, 1000);
+﻿// случайное трехзначное число и из него удалить вторую цифру и полуть двухзначное число из оставшихся чисел
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число {number}");
+//int firstDigit = number / 100;
+//int lastDigit = number % 10;
+//int result = firstDigit * 10 + lastDigit;
+//Console.WriteLine($"Результат вычислений {result}");
+
+
+
+
+int twoDigit = DigitSum(number);
+Console.WriteLine($"Результат вычислений {twoDigit}");
+
+int DigitSum(int num)// num = number
+{
+    int firstDigit = number / 100;
+    int lastDigit = number % 10;
+    int twoDigit = firstDigit * 10 + lastDigit;
+    return twoDigit;
+}
